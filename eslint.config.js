d0b7ea416e181628ed656fecffd85741ada6706f
HotSlug <hotslug@gmail.com>
2025-04-30 00:31:04 +0100

    chore: adds warning to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,14 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, prettier, {
+  plugins: {
+    react: reactPlugin,
+    'react-hooks': reactHooksPlugin,
+  },
   languageOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -14,5 +20,24 @@ export default tseslint.config(js.configs.recommended, ...tseslint.configs.recom
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'warn',
+    'no-duplicate-imports': 'error',
+    'no-unused-expressions': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'all'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
   },
 });
